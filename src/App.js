0b19e4403c208238305c23d74d8098e90f17c487
fakhriyya rahimova <fakhriyyarahimova@fakhriyyas-MBP.home>
2022-12-06 23:13:15 +0100

    increasing number game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+
+  const [count, setCount] = useState(0)
+
+  function increase(){
+    setCount(count+1)
+  }
+
+  function decrease(){
+    setCount(count-1)
+  }
+
+
+return(
+<div className="container">
+  <h1>{count}</h1>
+  <button onClick={increase}>+</button>
+  <button onClick={decrease}>-</button>
+</div>)
 }
-
 export default App;
+
+
